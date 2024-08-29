320df90598454a84484ac27410e943d9896c0508
francoAgustin07 <faluque@udc.edu.ar>
2024-08-29 11:24:59 -0300

    Creacion del modelo Mensaje con sus atributos correspondientes

--- TableroMensajes/mensajes/models.py
+++ TableroMensajes/mensajes/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
-# Create your models here.
+class Mensaje(models.Model):
+    texto = models.TextField()
+    remitente = models.CharField(max_length=100)
+    destinatario = models.CharField(max_length=100)
+    fecha_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Mensaje de {self.remitente} para {self.destinatario}'
